aa8f6ebe4a3679352e145b79c3aafd87f725a3c1
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2022-09-01 15:05:05 -0400

    CFT and API work as intended for now, need to connect this to the frontend

---
+++ cleanup.sh
@@ -0,0 +1,18 @@
+rm -rf certifi/
+rm -rf certifi-2022.6.15.dist-info
+rm -rf charset_normalizer
+rm -rf charset_normalizer-2.1.1.dist-info
+rm -rf idna
+rm -rf idna-3.3.dist-info
+rm -rf jwt/
+rm -rf PyJWT-2.4.0.dist-info
+rm -rf pytz
+rm -rf pytz-2022.2.1.dist-info
+rm -rf requests
+rm -rf requests-2.28.1.dist-info
+rm -rf twilio
+rm -rf twilio-7.13.0.dist-info
+rm -rf urllib3
+rm -rf urllib3-1.26.12.dist-info
+rm -rf .aws-sam
+rm out.yaml
---
+++ main.py
@@ -0,0 +1,46 @@
+# # Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+import json
+from base64 import b64decode
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+logging.basicConfig(format="%(asctime)s: %(levelname)s: %(message)s", level=logging.INFO)
+
+def lambda_handler(event, context):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    try:
+        event_body = json.loads(b64decode(event["body"]))
+        logging.info(event_body)
+        phone_number = event_body["phone_number"]
+        message = client.messages \
+                    .create(
+                        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                        from_="+19138008135",
+                        to=phone_number
+                    )
+        return {
+            "headers": {
+                "Access-Control-Allow-Origin": "https://d30oxzp0dgco37.cloudfront.net",
+                'Access-Control-Allow-Methods': 'POST'
+            },
+            "statusCode": 200,
+            "body": message.sid
+        }
+    # generic exception, though it might just be a KeyError
+    except Exception as e:
+        logging.error(str(e))
+        return {
+            "headers": {
+                "Access-Control-Allow-Origin": "https://d30oxzp0dgco37.cloudfront.net",
+                'Access-Control-Allow-Methods': 'POST'
+            },
+            "statusCode": 500,
+            "body": str(e)
+        }
